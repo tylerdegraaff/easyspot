899893700e3af81e84f64c82f7c6290f9f55663c
Damian van Straten <damianvanstraten@hotmail.com>
2016-05-18 16:23:01 +0200

    Dropdown erin gegooid

--- app/src/main/java/nl/hr/tylerdegraaff/easyspot/BookingCampingActivity.java
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/BookingCampingActivity.java
@@ -2,12 +2,29 @@ package nl.hr.tylerdegraaff.easyspot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
 
 public class BookingCampingActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> myArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_booking_camping);
+
+        spinner = (Spinner)findViewById(R.id.plotOptions);
+        myArrayAdapter = ArrayAdapter.createFromResource(this, R.array.dropdown_plot_options, android.R.layout.simple_spinner_item);
+        myArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(myArrayAdapter);
     }
 }
