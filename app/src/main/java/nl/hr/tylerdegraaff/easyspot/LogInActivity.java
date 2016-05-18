db5c2571ced736cdd675cb84db6df323276474cf
tylerdegraaff <tylerjdg_nl@hotmail.com>
2016-05-18 15:19:47 +0200

    goodbye actionbar

--- app/src/main/java/nl/hr/tylerdegraaff/easyspot/LogInActivity.java
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/LogInActivity.java
@@ -10,6 +10,7 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_log_in);
     }
 
