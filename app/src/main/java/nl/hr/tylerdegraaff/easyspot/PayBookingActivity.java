9ccaac42115712b6f206bf6a901f257243b1146a
tylerdegraaff <tylerjdg_nl@hotmail.com>
2016-05-19 00:53:10 +0200

    getting to the end... finally. flow of reservation is done and small bug fixes

--- app/src/main/java/nl/hr/tylerdegraaff/easyspot/PayBookingActivity.java
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/PayBookingActivity.java
@@ -4,18 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-public class SearchCampingActivity extends AppCompatActivity {
+public class PayBookingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_search_camping);
+        setContentView(R.layout.activity_pay_booking);
     }
 
-    public void Reservate(View v) {
-        Intent intent = new Intent(this, BookingKaraokeActivity.class);
+    public void onPay(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        Toast toast = Toast.makeText(this, "Payment has been confirmed", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
