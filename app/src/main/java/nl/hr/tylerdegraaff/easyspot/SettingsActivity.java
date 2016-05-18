4de4f9f38e50c1d54045053ab749dbbce97b3968
Damian van Straten <damianvanstraten@hotmail.com>
2016-05-19 00:03:19 +0200

    Settings page

---
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/SettingsActivity.java
@@ -0,0 +1,20 @@
+package nl.hr.tylerdegraaff.easyspot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public void AccountClick(View view)
+    {
+        startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+    }
+}
