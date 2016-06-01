312694ab62497e81a1be8b5ae0a812dc1e283ed3
Damian van Straten <damianvanstraten@hotmail.com>
2016-06-01 16:46:55 +0200

    Opstart scherm toegevoegd. Design en animatie kunnen veranderd nog worden

---
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/SplashScreenActivity.java
@@ -0,0 +1,46 @@
+package nl.hr.tylerdegraaff.easyspot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        imageView = (ImageView)findViewById(R.id.anim_logo);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_up);
+        imageView.setAnimation(animation);
+
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                startActivity(new Intent(getApplicationContext(),LogInActivity.class));
+
+                finish();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
