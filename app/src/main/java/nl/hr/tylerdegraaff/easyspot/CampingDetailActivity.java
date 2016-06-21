2f05bf6b6602c4c90825ddf0ee1a27402fbb700f
Damian van Straten <damianvanstraten@hotmail.com>
2016-06-22 00:52:09 +0200

    Camping detail activity (not finished)

--- app/src/main/java/nl/hr/tylerdegraaff/easyspot/CampingDetailActivity.java
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/CampingDetailActivity.java
@@ -1,21 +1,42 @@
 package nl.hr.tylerdegraaff.easyspot;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CampingDetailActivity extends AppCompatActivity {
+
+    String  _campingName, _campingAddress, _campingImage, _campingEmail, _campingFacilities, _campingDescription;;
+    int     _campingPhone, _campingPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_booking_karaoke);
+        setContentView(R.layout.activity_camping_detail);
+
+        Intent i = getIntent();
+        _campingName = i.getStringExtra("name");
+        _campingImage = i.getStringExtra("image");
+        _campingAddress = i.getStringExtra("address");
+
+        TextView tv = (TextView)findViewById(R.id.detail_camping_name);
+        tv.setText(_campingName);
+
+    }
+
+    public void Back(View view) {
+        finish();
     }
 
     public void bookNow(View v) {
-        Intent intent = new Intent(this, BookingGeneralActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(this, BookingGeneralActivity.class));
     }
+
 }
--- app/src/main/java/nl/hr/tylerdegraaff/easyspot/SearchCampingActivity.java
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/SearchCampingActivity.java
@@ -3,6 +3,7 @@ package nl.hr.tylerdegraaff.easyspot;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -29,12 +30,13 @@ public class SearchCampingActivity extends AppCompatActivity {
         campings = db.getAllCampings();
 
 //        for(Camping camping : campings) {
-//             db.deleteCamping(camping);
+//            db.deleteCamping(camping);
 //        }
 
         //db.addCamping(new Camping(1, "Camping Blue Sea", "France, Montpellier", R.drawable.camping1));
-        //db.addCamping(new Camping(1, "Camping Holland", "France, Nice", R.drawable.camping2));
-        //db.addCamping(new Camping(1, "Camping Karaoke", "France Marseille", R.drawable.camping3));
+        //db.addCamping(new Camping(2, "Camping Holland", "France, Nice", R.drawable.camping2));
+        //db.addCamping(new Camping(3, "Camping Karaoke", "France Marseille", R.drawable.camping3));
+
         createListView();
         registerClickCallback();
     }
@@ -56,44 +58,21 @@ public class SearchCampingActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View viewClicked,
                                     int position, long id) {
-                startActivity(new Intent(getApplicationContext(), CampingDetailActivity.class));
                 Camping clickedCamping = campings.get(position);
-                String message = "You clicked on camping " + clickedCamping.getName();
-                Toast.makeText(SearchCampingActivity.this, message, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(SearchCampingActivity.this, CampingDetailActivity.class);
+
+                String imageId = String.valueOf(clickedCamping.getImage());
+
+                intent.putExtra("name", clickedCamping.getName());
+                intent.putExtra("image", imageId);
+                intent.putExtra("address", clickedCamping.getAddress());
+
+                startActivity(intent);
+                String message = "You clicked on camping " + clickedCamping.getImage();
+                Toast.makeText(SearchCampingActivity.this, clickedCamping + message, Toast.LENGTH_LONG).show();
             }
         });
     }
-
-//    public void fillListViewWithCampings()
-//    {
-//        //db.addCamping(new Camping(1, "Camping Blue Sea", "France, Montpellier, Quteare zou 23"));
-//        //db.addCamping(new Camping(2, "Camping Holland", "France, Nice, Zaig veg qou 717"));
-//        //db.addCamping(new Camping(3, "Camping Karaoke", "France, Marseille, Kaloged-aouf 98"));
-//
-//        campingNameList = new ArrayList<String>();
-//        for (Camping camping : campings) {
-//
-//            //db.deleteCamping(camping);
-//
-//            // Log for debug use
-//            String log = "Id: " + camping.getId() + " ,Name: " + camping.getName() + " ,Address: " + camping.getAddress();
-//
-//            // Getting the name and adress
-//            String name = camping.getName();
-//            String adress = camping.getAddress();
-//
-//            // Add the camping to the list
-//            //campingNameList.add("Camping Id:" + camping.getId());
-//            campingNameList.add(name);
-//            campingNameList.add(adress);
-//        }
-//
-//        // Create The Adapter with passing ArrayList as 3rd parameter
-//        ArrayAdapter<String> arrayAdapter =
-//                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, campingNameList);
-//        // Set The Adapter
-//        campingList.setAdapter(arrayAdapter);
-//    }
 
     public class myListAdapter extends ArrayAdapter<Camping> {
         public myListAdapter() {
@@ -108,11 +87,6 @@ public class SearchCampingActivity extends AppCompatActivity {
             if (itemView == null) {
                 itemView = getLayoutInflater().inflate(R.layout.camping_listview, parent, false);
             }
-
-            // Loop trough all Campings en fill the TextViews
-                // Log for debug use
-                //String log = "Id: " + camping.getId() + " ,Name: " + camping.getName() + " ,Address: " + camping.getAddress();
-
                 Camping currentCamping = campings.get(position);
 
                 // Fill the view
