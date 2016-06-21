cdebb5784529b38f4394387ca0820af03d01fb03
Damian van Straten <damianvanstraten@hotmail.com>
2016-06-21 21:56:41 +0200

    Dynamic listview with campings + styled

--- app/src/main/java/nl/hr/tylerdegraaff/easyspot/ReservationDetailActivity.java
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/ReservationDetailActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class CampingKaraokeActivity extends AppCompatActivity {
+public class ReservationDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
