88262490246ab83f2ecfc5c22793e909bebfe552
tylerdegraaff <tylerjdg_nl@hotmail.com>
2016-06-21 20:48:30 +0200

    added extra column for images

--- app/src/main/java/nl/hr/tylerdegraaff/easyspot/Camping.java
+++ app/src/main/java/nl/hr/tylerdegraaff/easyspot/Camping.java
@@ -4,14 +4,16 @@ public class Camping {
     private int id;
     private String name;
     private String address;
+    private String image;
     public Camping()
     {
     }
-    public Camping(int id,String name,String address)
+    public Camping(int id,String name,String address, String image)
     {
         this.id=id;
         this.name=name;
         this.address=address;
+        this.image=image;
     }
     public void setId(int id) {
         this.id = id;
@@ -19,9 +21,11 @@ public class Camping {
     public void setName(String name) {
         this.name = name;
     }
-
     public void setAddress(String address) {
         this.address = address;
+    }
+    public void setImage(String image) {
+        this.image = image;
     }
     public int getId() {
         return id;
@@ -31,6 +35,9 @@ public class Camping {
     }
     public String getName() {
         return name;
+    }
+    public String getImage() {
+        return image;
     }
 }
 
